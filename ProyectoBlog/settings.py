4c1b88738dc992b502a72904fa78f0e906106d3c
David <97991928+zumaque13@users.noreply.github.com>
2022-02-15 13:08:54 +0100

    Update settings.py

--- ProyectoBlog/settings.py
+++ ProyectoBlog/settings.py
@@ -132,3 +132,11 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
+#Settings para el envio de correos desde contacto
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'blogtercertiempo2022@gmail.com'
+EMAIL_HOST_PASSWORD = 'zyv4BEM@xyx@azb9ete'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
