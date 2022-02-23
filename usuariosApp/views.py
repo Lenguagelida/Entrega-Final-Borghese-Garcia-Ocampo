16d83f0983071e42e189e15d33f082db24eb8194
Lenguagelida <dab777799@gmail.com>
2022-02-23 06:38:56 -0300

    Permisos usuario normal y admin completos

--- usuariosApp/views.py
+++ usuariosApp/views.py
@@ -1,80 +1,53 @@
-from urllib import request
-import django
 from django.shortcuts import redirect, render
-from django.db.models import Q
-
-
 # Para el login
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login, logout, authenticate
-from .forms import CreateUserForm
-from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from usuariosApp.forms import UserRegisterForm, UserEditForm
 from postApp.models import Post
-
-
-#Decorators
+from django.contrib.auth.models import Group
+# Decorators
 from postApp.decorators import unauthenticated_user, allowed_users
 from django.contrib.auth.decorators import login_required
-
 
 
 # Register
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
-
         if form.is_valid():
             username = form.cleaned_data['username']
-            form.save()
-
+            group = Group.objects.get(name='Comunidad')
+            user = form.save()
+            user.groups.add(group)
             return render(request, 'usuariosApp/bienvenido.html', {"mensaje": f"El usuario de {username} se ha creado con éxito"})
-
         else:
-
             return render(request, 'usuariosApp/bienvenido.html', {"mensaje": "El usuario no se ha creado"})
-
     else:
-
         form = UserRegisterForm()
         return render(request, 'usuariosApp/registro.html', {"form": form})
 
 
 # Login
 def login_request(request):
+
     if request.user.is_authenticated:
         return redirect('inicio')
     else:
         if request.method == "POST":
-
             form = AuthenticationForm(request, data=request.POST)
-
             if form.is_valid():
-
                 data = form.cleaned_data
-
                 user = authenticate(
                     username=data['username'], password=data['password'])
-
                 if user is not None:
-
                     login(request, user)
-
-                    # return redirect ('inicio2.html')
-
                     return render(request, "usuariosApp/bienvenido.html", {"mensaje": f"Bienvenido {user.get_username()}"})
-
                 else:
-
                     return render(request, "usuariosApp/login.html", {"mensaje": "Datos incorrectos"})
-
             else:
-
                 return render(request, "usuariosApp/login.html", {"mensaje": "Error en el formulario"})
-
         form = AuthenticationForm()
-
         return render(request, "usuariosApp/login.html", {'form': form})
 
 
@@ -87,9 +60,7 @@ def editarPerfil(request):
     if request.method == 'POST':
         miFormulario = UserEditForm(request.POST)
         if miFormulario.is_valid():  # Si pasó la validación de Django
-
             informacion = miFormulario.cleaned_data
-
             # Datos que se modificarán
             usuario.email = informacion['email']
             usuario.password1 = informacion['password1']
@@ -97,7 +68,6 @@ def editarPerfil(request):
             usuario.first_name = informacion['first_name']
             usuario.last_name = informacion['last_name']
             usuario.save()
-
             # Vuelvo al inicio o a donde quieran
             return render(request, "usuariosApp/bienvenido.html", {"mensaje": "Datos de perfil modificados correctamente"})
     # En caso que no sea post
@@ -105,19 +75,11 @@ def editarPerfil(request):
         # Creo el formulario con los datos que voy a modificar
         miFormulario = UserEditForm(initial={
                                     'email': usuario.email, 'first_name': usuario.first_name, 'last_name': usuario.last_name})
-
     # Voy al html que me permite editar
     return render(request, "usuariosApp/editar_perfil.html", {"miFormulario": miFormulario, "usuario": usuario})
 
 
-# logout
-# def logout_request(request):
-#     logout(request)
-#     return redirect('login')
-
-
-
 def postsConMeGustaPropios(request):
     current_user = request.user.id
-    posts=Post.objects.filter(likes__id = current_user)
-    return render(request,'usuariosApp/posts_con_megusta.html',{"posts":posts})
+    posts = Post.objects.filter(likes__id=current_user)
+    return render(request, 'usuariosApp/posts_con_megusta.html', {"posts": posts})
